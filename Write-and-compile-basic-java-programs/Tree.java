60e80dd9abb96d8dc5a1bedb43e97b7680951762
signaajames <penguingnop@gmail.com>
2026-04-10 10:43:36 +0800

    Plant a tree task in Codecademy

---
+++ Write-and-compile-basic-java-programs/Tree.java
@@ -0,0 +1,16 @@
+public class Tree {
+    public static void main(String[] args) {
+        // I am doctor james and we will plant a tree
+        System.out.println("I'm Dr.James and I'm learning to code in Java!");
+        System.out.println("And we're going to plant a tree in the terminal");
+        System.out.println("       ###\n" +
+                "      #####\n" +
+                "    #########\n" +
+                "   ###\\#|#/###\n" +
+                "    ###\\|/###\n" +
+                "     # }|{ # \n" +
+                "       }|{\n" +
+                "       }|{");
+
+    }
+}
